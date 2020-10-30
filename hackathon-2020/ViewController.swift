3d54855017abe2296315451e82fe6a98f946b87b
Artem Rylov <tema2707@icloud.com>
2020-10-30 20:25:16 +0700

    Initial project

---
+++ hackathon-2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hackathon-2020
+//
+//  Created by art-off on 30.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
